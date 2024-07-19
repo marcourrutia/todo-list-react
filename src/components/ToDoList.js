eb6616ebf98a8bbbbae82c04662b6425424e6678
Marco Andres Urrutia Mora <maurrutiam@minsal.cl>
2024-07-19 12:57:30 -0400

    logica de añadir tareas

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,35 @@
 import "./ToDoList.css";
+import { useState } from "react";
 
 export const ToDoList = () => {
+  const [inputText, setInputText] = useState("");
+  const [list, setList] = useState("");
+
+  const ul = document.querySelector("#list");
+  const newLi = document.createElement("li");
+
+  const handleOnChange = (event) => {    
+    setInputText(event.target.value);
+  };
+  const handleOnKeyDown = (event) => {
+    if (event.keyCode === 13 && inputText != "") {
+      newLi.innerHTML = inputText;
+      ul.append(newLi);
+      setInputText("");
+    }
+  };
+  
   return (
     <>
-      <h1>TAREAS</h1>
+      <h1>MIS TAREAS</h1>
       <div className="container">
-        <input placeholder="Ingresa una tarea"></input>
-        <ul>
-            <li>tarea</li>
+        <input
+          placeholder="Ingresa una tarea"
+          onChange={handleOnChange}
+          onKeyDown={handleOnKeyDown}
+          value={inputText}
+        />
+        <ul id="list">
         </ul>
         <p>Tareas pendientes: 1</p>
       </div>
